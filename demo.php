c3c18998f6894376da0d8383f11ca2c6ab068079
Rana Faraz <ranafarazahmed@gmail.com>
2020-03-29 15:27:54 +0500

    Called the contrcuct method and displayed the results.

--- demo.php
+++ demo.php
@@ -31,3 +31,13 @@ echo "Address ID: {$address->_address_id}";
 echo '<h2>Testing magic __get and __set</h2>';
 unset($address->postal_code);
 echo $address->display();
+
+echo '<h2>Testing Address construct with an array</h2>';
+$address_2 = new Address(array(
+  'street_address_1' => '123 Stallite Town',
+  'city_name' => 'RYK',
+  'subdivision_name' => 'Region',
+  'postal_code' => '32432',
+  'country_name' => 'Pakistan',
+));
+echo $address_2->display();
