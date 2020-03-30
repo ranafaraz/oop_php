454eb265bbbe4c48d5fa8f1ffeea0d9eaf16695d
Rana Faraz <ranafarazahmed@gmail.com>
2020-03-30 13:22:13 +0500

    Index page for this practice camp.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="demo.php">Demo without Database Class</a>
+        <a href="demo2.php">Demo with Database Calss</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
