fe446a5b1d1e188e3135d5ea875fcebba86aa12b
Rana Faraz <ranafarazahmed@gmail.com>
2020-03-30 15:47:08 +0500

    Abstraction implemented.

--- demo2.php
+++ demo2.php
@@ -1,29 +1,37 @@
 <?php
 
-require 'class.Address.inc';
-require 'class.Database.inc';
+//require 'class.Address.inc';
+//require 'class.Database.inc';
 
-echo '<h2>Instantiating Address';
-$address = new Address;
+/**
+ * Define autoloader
+ * @param string $class_name
+ */
+function __autoload($class_name){
+    include 'class.' . $class_name . '.inc';
+}
+
+echo '<h2>Instantiating AddressResidence';
+$address_residence = new AddressResidence();
 
 
 // Assigning values to the properties of address class.
 echo "<h2>Setting Properties</h2>";
-$address->street_address_1 = '123 Fake Street';
-$address->street_address_2 = 'Bla Bla Bla';
-$address->city_name = 'Townsville';
-$address->subdivision_name = 'State';
-$address->country_name = 'United States of America';
-$address->address_type_id = 1;
+$address_residence->street_address_1 = '123 Fake Street';
+$address_residence->street_address_2 = 'Bla Bla Bla';
+$address_residence->city_name = 'Townsville';
+$address_residence->subdivision_name = 'State';
+$address_residence->country_name = 'United States of America';
+echo $address_residence;
 
-echo $address;
-
+echo '<tt><pre>' . var_export($address_residence, TRUE) . '</pre></tt>';
 
 echo '<h2>Testing Address construct with an array</h2>';
-$address_2 = new Address(array(
+$address_business = new AddressBusiness(array(
   'street_address_1' => '123 Stallite Town',
   'city_name' => 'Villageland',
   'subdivision_name' => 'Region',
   'country_name' => 'Canada',
 ));
-echo $address_2;
+echo $address_business;
+echo '<tt><pre>' . var_export($address_business, TRUE) . '</pre></tt>';
